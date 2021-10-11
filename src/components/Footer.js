07369e5a8483d2dcf0dcb5f79d40dc349a844455
Prince Jain <princejainprince.pj@gmail.com>
2021-10-11 18:13:45 +0530

    update Commit

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -1,55 +1,53 @@
-import React from 'react'
+import React from "react";
 
-const Footer=(price)=> {
+const Footer = (price) => {
+  if (price !== "0.00") {
+    return (
+      <div className="footer">
+        Designed and Developed by © Prince Jain
+        <div>
+          <span className="links">
+            <a
+              href="https://www.linkedin.com/in/nishant-kumar-mishra-8503a9122/"
+              target="_blank"
+              rel="noreferrer"
+            >
+              <span>LinkedIn</span>
+            </a>
+          </span>
+          <span className="links">
+            <a
+              href="https://twitter.com/nishantmishra19"
+              target="_blank"
+              rel="noreferrer"
+            >
+              <span> Twitter</span>
+            </a>
+          </span>
 
-    if (price!=="0.00"){
-        return<div className="footer">Designed and Developed by © Nishant Mishra 
-        <div  >
-            <span className="links" >
-              <a
-                href="https://www.linkedin.com/in/nishant-kumar-mishra-8503a9122/"
-                target="_blank"
-                rel="noreferrer"
-              >
-                <span>LinkedIn
-                </span>
-              </a>
-            </span>
-            <span className="links" >
-              <a
-                href="https://twitter.com/nishantmishra19"
-                target="_blank"
-                rel="noreferrer"
-              >
-                <span > Twitter
-                </span>
-              </a>
-            </span>
+          <span className="links">
+            <a
+              href="https://github.com/nishant1120"
+              target="_blank"
+              rel="noreferrer"
+            >
+              <span>GitHub</span>
+            </a>
+          </span>
 
-            <span className="links">
-              <a
-                href="https://github.com/nishant1120"
-                target="_blank"
-                rel="noreferrer"
-              >
-                <span >GitHub
-                </span>
-              </a>
-            </span>
+          <span className="links">
+            <a
+              href="https://nishantmishra.vercel.app/"
+              target="_blank"
+              rel="noreferrer"
+            >
+              <span>Portfolio</span>
+            </a>
+          </span>
+        </div>
+      </div>
+    );
+  }
+};
 
-            <span className="links">
-              <a
-                href="https://nishantmishra.vercel.app/"
-                target="_blank"
-                rel="noreferrer"
-              >
-                <span >Portfolio
-                </span>
-              </a>
-            </span>
-          </div>
-</div>    
-    }
-}
-
-export default Footer
+export default Footer;
